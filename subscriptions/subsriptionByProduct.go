bbb6cb1637e63c792e86b8960584d358f6e4d0a2
Pramodh <pramodvallanur@yahoo.com>
2020-03-31 19:41:06 -0700

    Update subsriptionByProduct.go

--- subscriptions/subsriptionByProduct.go
+++ subscriptions/subsriptionByProduct.go
@@ -91,7 +91,7 @@ func main() {
     subscriptionRequest, err := http.NewRequest("GET",SubscriptionUrl, nil)      
 	subscriptionRequest.Header.Set("Content-type","application/json")
 	//TODO: Replace the bearer token
-    subscriptionRequest.Header.Set("Authorization", "Bearer token")
+    subscriptionRequest.Header.Set("Authorization", "Bearer token redacted for security reasons. To get the bearer token use token login")
     if err != nil{
       log.Fatalln(err)
     }
@@ -100,14 +100,14 @@ func main() {
     if err != nil{
       log.Fatalln(err)
 	}
-	//Parse the response
+    //Parse the response
     var subscriptionObj subscription
     json.NewDecoder(subscriptionResp.Body).Decode(&subscriptionObj)  	
     //Get the consumer object
     consumerOrgRequest, err := http.NewRequest("GET",subscriptionObj.ConsumerOrgURL, nil)      
     consumerOrgRequest.Header.Set("Content-type","application/json")
     //TODO: Replace the bearer token
-    consumerOrgRequest.Header.Set("Authorization", "Bearer token")
+    consumerOrgRequest.Header.Set("Authorization", "Bearer token redacted for security reasons. To get the bearer token use token login")
     if err != nil{
       log.Fatalln(err)
     }
@@ -124,7 +124,7 @@ func main() {
     //Get the application object
     appRequest, err := http.NewRequest("GET",subscriptionObj.AppURL, nil)      
     appRequest.Header.Set("Content-type","application/json")
-    appRequest.Header.Set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkODVkNzM3MS01NDhiLTQ2Y2UtYjdkOC01MWQ5NjUwOTY2YWMiLCJuYW1lc3BhY2UiOiI2ODgwOTczYi0yZGY1LTRlYTItODMzMC01OTM4NjYxYTk3NmM6ZjBiMTM5MzItYTU3OS00NGZiLWFlMTEtMGJhZWY4MmY2YjQ4OmZlMGNjY2RkLTZhZWQtNGM2ZC1hNzYzLWEwNjdjNjQwOTNiOSIsImF1ZCI6Ii9hcGkvY2xvdWQvcmVnaXN0cmF0aW9ucy8xNzMzYThhNi0xYjY0LTRiYWMtOTJhNS1iNDg5OTU0MjU0NDIiLCJzdWIiOiIvYXBpL3VzZXItcmVnaXN0cmllcy82ODgwOTczYi0yZGY1LTRlYTItODMzMC01OTM4NjYxYTk3NmMvZjBiMTM5MzItYTU3OS00NGZiLWFlMTEtMGJhZWY4MmY2YjQ4L3VzZXJzL2ZlMGNjY2RkLTZhZWQtNGM2ZC1hNzYzLWEwNjdjNjQwOTNiOSIsImlzcyI6IklCTSBBUEkgQ29ubmVjdCIsImV4cCI6MTU4NTcwOTc5MCwiaWF0IjoxNTg1NjgwOTkwLCJncmFudF90eXBlIjoicGFzc3dvcmQiLCJ1c2VyX3JlZ2lzdHJ5X3VybCI6Ii9hcGkvdXNlci1yZWdpc3RyaWVzLzY4ODA5NzNiLTJkZjUtNGVhMi04MzMwLTU5Mzg2NjFhOTc2Yy9mMGIxMzkzMi1hNTc5LTQ0ZmItYWUxMS0wYmFlZjgyZjZiNDgiLCJyZWFsbSI6InByb3ZpZGVyL2RlZmF1bHQtaWRwLTIiLCJ1c2VybmFtZSI6InN0ZXZlIiwiaWRfdG9rZW4iOiJleUpoYkdjaU9pSklVekkxTmlJc0luUjVjQ0k2SWtwWFZDSjkuZXlKbWFYSnpkRjl1WVcxbElqb2lVM1JsZG1VaUxDSnNZWE4wWDI1aGJXVWlPaUpQZDI1bGNpSXNJblZ6WlhKZmFXUWlPaUl4TmpSalptWXdaaTAwTnpWa0xUUXpOelF0T1RRMll5MWpNbUZsWm1GbU1ERTROek1pTENKMWMyVnlibUZ0WlNJNkluTjBaWFpsSWl3aWFXRjBJam94TlRnMU5qZ3dPVGt3ZlEucFBPMU1JbVMzR2U2ZmZnOWZCREJybTloWVBVQ0xlMm1qTFp6MEhnWkxrRSIsInNjb3BlcyI6WyJjbG91ZDp2aWV3IiwiY2xvdWQ6bWFuYWdlIiwicHJvdmlkZXItb3JnOnZpZXciLCJwcm92aWRlci1vcmc6bWFuYWdlIiwib3JnOnZpZXciLCJvcmc6bWFuYWdlIiwiZHJhZnRzOnZpZXciLCJkcmFmdHM6ZWRpdCIsImNoaWxkOnZpZXciLCJjaGlsZDpjcmVhdGUiLCJjaGlsZDptYW5hZ2UiLCJwcm9kdWN0OnZpZXciLCJwcm9kdWN0OnN0YWdlIiwicHJvZHVjdDptYW5hZ2UiLCJhcHByb3ZhbDp2aWV3IiwiYXBwcm92YWw6bWFuYWdlIiwiYXBpLWFuYWx5dGljczp2aWV3IiwiYXBpLWFuYWx5dGljczptYW5hZ2UiLCJjb25zdW1lci1vcmc6dmlldyIsImNvbnN1bWVyLW9yZzptYW5hZ2UiLCJhcHA6dmlldzphbGwiLCJhcHA6bWFuYWdlOmFsbCIsIm15OnZpZXciLCJteTptYW5hZ2UiLCJ3ZWJob29rOnZpZXciXX0.KI0IxbYzY9u2ywnEB0lIRFJsQvdiPtMhguYsWNjQq-o")
+    appRequest.Header.Set("Authorization", "Bearer Bearer token redacted for security reasons. To get the bearer token use token login")
     if err != nil{
       log.Fatalln(err)
     }
